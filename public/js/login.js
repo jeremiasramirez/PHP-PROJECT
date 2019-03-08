7b34ea5264ee7da8429d5de2a8bf59db793e4959
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-08 07:51:24 -0400

    add login and register

--- public/js/login.js
+++ public/js/login.js
@@ -1,1 +1,9 @@
+
+
+let formData = document.getElementById("form__data--js");
+let listInput = formData.querySelectorAll("input");
+	let email = listInput[0].value = "myuser@email.com";
+	let pass = listInput[1].value = ".......";
+
+ 	 
  
