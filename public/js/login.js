f60667704ba145930b3508b2c7978038d27e59e1
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-18 16:38:27 -0400

    Creando la clase de validacion del formulario en frontend - js

--- public/js/login.js
+++ public/js/login.js
@@ -1,11 +1,42 @@
-
-
 let linkLogin = document.getElementById("menu__nav__list--js");
  
-	linkLogin.addEventListener("click",(e)=>{
-	 
-		linkLogin.classList.add("scale");
+(function animationButtonLogin(linkLogin){
+ linkLogin.addEventListener("click",(e)=>{
+ 	linkLogin.classList.add("scale");
+ },false);
+})(linkLogin)
 
-	},false);
- 	 
- 
+let buttonSendForm = document.getElementById("btn__send--js");
+
+class validateLogin{
+
+	constructor(email,password){
+		this.email = email;
+		this.password = password;
+	}
+	validate(){
+
+	}
+	noValidate(){
+		
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
