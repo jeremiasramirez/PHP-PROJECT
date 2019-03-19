6c6a31ace27dc19e0d8b7827dc98ee907a450acf
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-18 20:48:01 -0400

    Mejorando las validaciones del lado front-end

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
+ 
 let linkLogin = document.getElementById("menu__nav__list--js");
  
 (function animationButtonLogin(linkLogin){
@@ -6,23 +7,85 @@ let linkLogin = document.getElementById("menu__nav__list--js");
  },false);
 })(linkLogin)
 
-let buttonSendForm = document.getElementById("btn__send--js");
+let buttonLoginForm = document.getElementById("btn__send--js");
+let buttonRegisterForm = document.getElementById("register");
 
-class validateLogin{
+
+
+class validateLoginForm{
 
 	constructor(email,password){
 		this.email = email;
 		this.password = password;
 	}
+
 	validate(){
 
 	}
+
 	noValidate(){
 		
 	}
 
+}
+
+let emailInput = document.getElementById("input__email--js");
+let passInput = document.getElementById("input__password--js");
+
+
+
+
+function clickLogin(emailInput ,passInput, btnSend){
+ 
+		emailInput.addEventListener("keyup",(e)=>{
+			let valWord = [emailInput.value];
+
+			let reference = ['-','}','{','+'
+							,'*','!','@','#',
+							'$','%','^','&',
+							'(',')','/','<','>',
+							 '=', '?', '~', '`',
+							 ':', ';'];
+			 if(emailInput.value.substring(0,1) === "_"){
+				 btnSend.disabled="true"
+				console.log("aqui");
+				btnSend.classList.add("btn__error");
+			 }
+			 else{
+				btnSend.disabled=""
+				btnSend.classList.remove("btn__error");
+				  
+			 }
+		
+ 
+	},false);
+
+	passInput.addEventListener("keyup",(e)=>{
+		 
+ 
+		 if(passInput.value.length < 7){
+			 btnSend.disabled="true"
+			console.log("aqui");
+			btnSend.classList.add("btn__error");
+		 }
+		 else{
+			btnSend.disabled="";
+			btnSend.classList.remove("btn__error");
+		 
+		 }
+	
+
+},false);
+
+	 
+ 
+
+
+
 
 }
+
+clickLogin(emailInput,passInput,buttonLoginForm);
 
 
 
