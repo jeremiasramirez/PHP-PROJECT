eb52bf75160d4ae21173e744faae8aed21ffed19
Jeremias ramirez <jereramirez499@gmail.com>
2019-03-18 22:33:44 -0400

    Arreglos en el frontend

---
+++ models/conection.php
@@ -0,0 +1,16 @@
+<?php
+
+
+function conectedDatabase(){
+	$conexion = '';
+	try{
+		$conexion = new mysqli("127.0.0.1","root","","firstdb");
+		echo "<h2>Hay conexion</h2>";
+	}catch(Exception $e){
+		echo $e->getMessage() . PHP_EOL;
+	}
+}
+
+conectedDatabase();
+
+
--- public/js/login.js
+++ public/js/login.js
@@ -7,6 +7,10 @@ let linkLogin = document.getElementById("menu__nav__list--js");
  },false);
 })(linkLogin)
 
+
+
+
+
 let buttonLoginForm = document.getElementById("btn__send--js");
 let buttonRegisterForm = document.getElementById("register");
 
@@ -14,29 +18,26 @@ let buttonRegisterForm = document.getElementById("register");
 
 class validateLoginForm{
 
-	constructor(email,password){
-		this.email = email;
-		this.password = password;
+	constructor(container,item){
+		this.container = container;
+		this.item = item;
 	}
 
 	validate(){
-
-	}
-
-	noValidate(){
 		
 	}
 
+	noValidate(){
+	 
+	}
 }
 
 let emailInput = document.getElementById("input__email--js");
 let passInput = document.getElementById("input__password--js");
 
-
-
-
 function clickLogin(emailInput ,passInput, btnSend){
- 
+ let band = false;
+ let band2 = false;
 		emailInput.addEventListener("keyup",(e)=>{
 			let valWord = [emailInput.value];
 
@@ -48,10 +49,11 @@ function clickLogin(emailInput ,passInput, btnSend){
 							 ':', ';'];
 			 if(emailInput.value.substring(0,1) === "_"){
 				 btnSend.disabled="true"
-				console.log("aqui");
+				 
 				btnSend.classList.add("btn__error");
 			 }
 			 else{
+				band = true;
 				btnSend.disabled=""
 				btnSend.classList.remove("btn__error");
 				  
@@ -65,15 +67,27 @@ function clickLogin(emailInput ,passInput, btnSend){
  
 		 if(passInput.value.length < 7){
 			 btnSend.disabled="true"
-			console.log("aqui");
+		 
 			btnSend.classList.add("btn__error");
 		 }
 		 else{
 			btnSend.disabled="";
 			btnSend.classList.remove("btn__error");
-		 
+			band2 = true;
 		 }
-	
+		 let containerV = document.getElementById("container__validate--js");
+		 let itemV = document.getElementById("container__item__js");
+		 let Validate = new validateLoginForm(containerV ,itemV);
+	if(band==true && band2==true){
+	 
+			Validate.validate();
+	}
+	else if(band==true && band2==false || band2==true && band==false){
+		btnSend.disabled="true"
+		 
+			btnSend.classList.add("btn__error");
+		Validate.noValidate();
+	}
 
 },false);
 
